6461c6d8a7c63ca1a63328d8cf2f9cd9a3cc5973
rayca-kathelen <rayca2433@gmail.com>
2025-09-25 14:18:45 -0300

    25/09

---
+++ exercicio1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Exercício de php</h1>
+
+    <form action="exercício1a.php">
+
+    <p> 
+        Digite o nome do cliente<br>
+        <input type="text" name="cliente">
+
+</p>
+
+<p>
+    Escolha o sexo do cliente <br>
+    <input type= "radio" name ="sexo" value="Feminino">Feminino<br>
+    <input type= "radio" name ="sexo" value="Masculino">masculino
+
+</p>
+
+<p>
+    Digite o valor da compra<br>
+    <input type="number" name="valorcompra">
+</p>
+
+<p>
+    <input type="submit" value="Enviar">
+</p>
+
+</form>
+</body>
+</html>
