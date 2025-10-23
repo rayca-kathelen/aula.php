b7e1a9905720b82c776c9fa45f978f09393226c8
rayca-kathelen <rayca2433@gmail.com>
2025-10-23 14:19:57 -0300

    23/10

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+   <?php include "menu.php"?>
+   
+    <div class="conteudo">
+        <h3>Integração de PHP com MySQL</h3>
+
+        <p>
+            Na aula de hoje,vamos estudar como integrar o PHP com MySQL
+        </p>
+    </div>
+    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,10 @@
+<div class="menu">
+        <div>
+            Aula BD
+        </div>
+
+        <div>
+            <a href="aluno_cadastro.php">Cadastrar</a>
+            <a href="#">Consultar</a>
+        </div>
+    </div>
